005156ef146ff7339f43e138ec0512305585caa7
Zulfiqar Alam <zulfiqaralam651@gmail.com>
2024-02-26 12:24:39 +0500

    Loops added

--- cal.py
+++ cal.py
@@ -1,16 +1,21 @@
+while True:
+    num = input("Enter first number ('exit' to quite'):")
+    if num.lower()=="exit":
+        print("Exiting the Screen")
+        break
+    op = input("Enter Operator(*,/,+,-) :")
+    num2 = int(input("Enter second number:"))
+    
+    num1 = int(num)
+    if op == '+':
+        print(num1+num2)
+    
+    elif op == '-':
+        print(num1-num2)
+    elif op == '/':
+        print(num1/num2)
+    elif op == '*':
+        print(num1*num2)
+    else:
+        print("Invalid Operator")
 
-num1 = int(input("Enter first number :"))
-op = input("Enter Operator(*,/,+,-) :")
-num2 = int(input("Enter second number:"))
-
-
-if op == '+':
-    print(num1+num2)
-elif op == '-':
-    print(num1-num2)
-elif op == '/':
-    print(num1/num2)
-elif op == '*':
-    print(num1*num2)
-else:
-    print("Invalid Operator")
