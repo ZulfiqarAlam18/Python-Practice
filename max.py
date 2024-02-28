45e9f38f94fe435f41b90ffb9b2b1eb5fd65a7e8
Zulfiqar Alam <zulfiqaralam651@gmail.com>
2024-02-28 08:28:33 +0500

    Small logic  modifications

---
+++ max.py
@@ -0,0 +1,10 @@
+#Maximum and minimum number in a list
+
+def min_max(list):
+    """Program to find max and min  number in a list"""
+    return f"Maximum vale is:{max(list)} \nMinimum value is:{min(list)}"
+
+
+
+print(min_max([1,3,6,11,66]))
+
--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,9 @@
 def is_palindrome(s):
     return s == s[::-1]
 
-print(is_palindrome("madam"))  
+str = input("Enter your Word that you want to check:")
+
+if is_palindrome(str) == True:
+    print(f"{str} is a palindrome")
+else:
+    print(f"{str} is not a palindrome")
